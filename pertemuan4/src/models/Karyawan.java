b6205c3ce0cd72685f26f7347bea09f2770d541a
Nathalie <nathalie.tantiwijaya@gmail.com>
2025-10-11 03:53:59 +0700

    ngerjain karyawan

---
+++ pertemuan4/src/models/Karyawan.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Karyawan extends Staff {
+    private int salary;
+    private ArrayList<PresensiStaff> presensiStaff;
+
+    public Karyawan(String nik, String nama, String alamat, String tempatLahir, String tanggalLahir, String telepon) {
+        super(nik, nama, alamat, tempatLahir, tanggalLahir, telepon);
+        this.salary = salary;
+        this.presensiStaff = new ArrayList<>();
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;;
+    }
+
+    public ArrayList<PresensiStaff> getPresensiStaff() {
+        return this.presensiStaff;
+    }
+
+
+}
